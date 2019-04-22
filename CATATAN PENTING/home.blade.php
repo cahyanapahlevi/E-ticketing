f288ae5c80aa7b820adabe38d471cffa9e6000bf
Rita Putri Saraswati <ritacancer.riserio@gmail.com>
2019-04-22 15:08:03 +0700

    Carousel Tampilan Programmer

---
+++ CATATAN PENTING/home.blade.php
@@ -0,0 +1,131 @@
+@extends('programmer.master')
+
+@section('programmer.content')
+        <div class="content-wrapper">
+		  <div class="row">
+            <div class="col-lg-12 grid-margin">
+              <div class="card">
+                <div class="card-body">
+				<div id="demo" class="carousel slide" data-ride="carousel" data-interval="1000">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+    <li data-target="#demo" data-slide-to="4"></li>
+	<li data-target="#demo" data-slide-to="5"></li>
+	<li data-target="#demo" data-slide-to="6"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{asset('source/images/kominfo2.png')}}" alt="Los Angeles" width="1000" height="500">
+      <div class="carousel-caption">
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('source/images/kominfo3.png')}}" alt="Chicago" width="1000" height="500">
+      <div class="carousel-caption">
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('source/images/kominfo4.png')}}" alt="New York" width="1000" height="500">
+      <div class="carousel-caption">
+      </div>   
+    </div>
+	<div class="carousel-item">
+      <img src="{{asset('source/images/kominfo5.png')}}" alt="New York" width="1000" height="500">
+      <div class="carousel-caption">
+      </div>   
+    </div>
+	<div class="carousel-item">
+      <img src="{{asset('source/images/kominfo6.png')}}" alt="New York" width="1000" height="500">
+      <div class="carousel-caption">
+      </div>   
+    </div>
+	<div class="carousel-item">
+      <img src="{{asset('source/images/kominfo8.png')}}" alt="New York" width="1000" height="500">
+      <div class="carousel-caption">
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+		
+          <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 grid-margin stretch-card">
+              <div class="card card-statistics">
+                <div class="card-body">
+                  <div class="clearfix">
+                    <div class="float-left">
+                      <i class="mdi mdi-cube text-danger icon-lg"></i>
+                    </div>
+                    <div class="float-right">
+                      <p class="mb-0 text-right">Project</p>
+                      <div class="fluid-container">
+                        <h3 class="font-weight-medium text-right mb-0">5</h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 grid-margin stretch-card">
+              <div class="card card-statistics">
+                <div class="card-body">
+                  <div class="clearfix">
+                    <div class="float-left">
+                      <i class="mdi mdi-receipt text-warning icon-lg"></i>
+                    </div>
+                    <div class="float-right">
+                      <p class="mb-0 text-right">On Progress</p>
+                      <div class="fluid-container">
+                        <h3 class="font-weight-medium text-right mb-0">10</h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 grid-margin stretch-card">
+              <div class="card card-statistics">
+                <div class="card-body">
+                  <div class="clearfix">
+                    <div class="float-left">
+                      <i class="mdi mdi-account-location text-info icon-lg"></i>
+                    </div>
+                    <div class="float-right">
+                      <p class="mb-0 text-right">Waiting List</p>
+                      <div class="fluid-container">
+                        <h3 class="font-weight-medium text-right mb-0">30</h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Tambahkan Project Yang Saat Ini Sedang Anada Kerjakan</h4>
+                  <div class="table-responsive">
+                    <table class="table table-bordered">
+                      <a href="{{url('/programmer/dproject')}}"><button type="button" class="btn btn-outline-success">Tambah Project</button></a>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+@endsection
---
+++ CATATAN PENTING/master.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Ticket Diskominfo</title>
+  <link rel="stylesheet" href="{{asset('source/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('source/vendors/iconfonts/mdi/css/materialdesignicons.min.css')}}">
+   <link rel="stylesheet" href="{{asset('source/vendors/iconfonts/font-awesome/css/font-awesome.css')}}">
+  <link rel="stylesheet" href="{{asset('source/vendors/css/vendor.bundle.base.css')}}">
+  <link rel="stylesheet" href="{{asset('source/vendors/css/vendor.bundle.addons.css')}}">
+  <link rel="stylesheet" href="{{asset('source/css/style.css')}}">
+  <link rel="shortcut icon" href="{{asset('source/images/coco.png')}}" />
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 100%;
+  }
+  </style>
+</head>
+
+<body>
+  <div class="container-scroller">
+
+  	<!-- untuk memanggil header -->
+  	@include('programmer.template.header')
+    
+    <div class="container-fluid page-body-wrapper">
+
+    	<!-- untuk memanggil sidebar (menu samping) -->
+      @include('programmer.template.sidebar')
+
+      <div class="main-panel">
+
+      	<!-- untuk memanggil isi halaman -->
+        @yield('programmer.content')
+
+        <!-- untuk memanggil footer -->
+        @include('programmer.template.footer')
+
+      </div>
+    </div>
+  </div>
+  
+   
+  <script src="{{asset('source/bootstrap/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('source/vendors/js/vendor.bundle.base.js')}}"></script>
+  <script src="{{asset('source/vendors/js/vendor.bundle.addons.js')}}"></script>
+  <script src="{{asset('source/js/off-canvas.js')}}"></script>
+  <script src="{{asset('source/js/misc.js')}}"></script>
+  <script src="{{asset('source/js/dashboard.js')}}"></script>
+  <script>
+  $(function () {
+    $('.carousel').carousel({ interval: 2000 })
+    $('#cycle').click(function() {
+      $('.carousel').carousel('cycle')
+    })
+    $('#pause').click(function() {
+      $('.carousel').carousel('pause')
+    })
+  })
+</script>
+   
+</body>
+
+</html>
